b404960223866d80bb4dbbb2857be3c2cca330e0
harshitha-nagaraju <160208357+harshitha-nagaraju@users.noreply.github.com>
2025-12-04 16:02:44 +0530

    Update seed_data.py

--- seed_data.py
+++ seed_data.py
@@ -4,8 +4,8 @@ from app.models import User, Category
 
 app = create_app()
 with app.app_context():
-if not User.query.filter_by(email='demo@example.com').first():
-u = User(name='Demo', email='demo@example.com')
+if not User.query.filter_by(email='harshithaan2004@gmail.com').first():
+u = User(name='Demo', email='harshithaan2004@gmail.com')
 db.session.add(u)
 for cat in ['Food','Transport','Entertainment','Rent','Utilities']:
 if not Category.query.filter_by(name=cat).first():
